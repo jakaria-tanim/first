e15b45bc332720c2112f722d2e114415771d27b0
jakaria-tanim <jakariaahmodtanim74@gmail.com>
2022-10-15 11:33:43 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+//MENU
+function openMenu(){
+    document.getElementById('menu-section').style.width="100%";  
+}
+function closeMenu(){
+    document.getElementById('menu-section').style.width="0";  
+}
+
+//MIX IT UP
+var mixer = mixitup('.work-grid');
+//WOW
+new WOW().init();
